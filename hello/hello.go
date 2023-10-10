77d95d6656f5353bbcef5d629c817f6ffc768f77
williamjpriest <liuliu6wp@gmail.com>
2023-10-10 08:36:00 -0400

    added randomness

--- hello/hello.go
+++ hello/hello.go
@@ -12,7 +12,7 @@ func main() {
 	log.SetPrefix(("greetings:"))
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("g")
+	message, err := greetings.Hello("G")
 	if err != nil{
 		log.Fatal((err))
 	}
